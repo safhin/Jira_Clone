bf5fb9fe20defeeff5d1c7e7818f4150e81fad3e
safhin <mdjahidhasan920@gmail.com>
2022-11-07 19:30:37 +0600

    Uknown changes

--- src/components/TaskTab.jsx
+++ src/components/TaskTab.jsx
@@ -3,35 +3,40 @@ import { useTodo } from "../context/TodoContext";
 import TabHeader from "./TabHeader";
 import Task from "./Task";
 
-const TaskTab = ({ title, tasks, handleDragging, isDraging, handleUpdatedTask }) => {
-  
+const TaskTab = ({
+  title,
+  tasks,
+  handleDragging,
+  isDraging,
+  handleUpdatedTask,
+}) => {
   const { todos, loading } = useTodo();
 
   const handleDragOver = (e) => {
     e.preventDefault();
-  }
+  };
 
   const handleDragDrop = (e) => {
     e.preventDefault();
-    console.log(e.target.closest('.kanban_item'))
-    const id = +e.dataTransfer.getData('text');
-    handleUpdatedTask(id,title)
-    handleDragging(false)
-  }
+    const id = +e.dataTransfer.getData("text");
+    handleUpdatedTask(id, title);
+    handleDragging(false);
+  };
 
   return (
-    <div className="flex flex-col w-1/3 ">
+    <div className="flex flex-col w-1/3 bg-gray-800 p-4">
       <TabHeader totalTasks={5} title={title.toUpperCase()} />
-      <div 
-        className="flex flex-col pb-2 overflow-auto h-screen kanban_container"
+      <div
+        className="flex flex-col pb-2 overflow-auto h-screen"
         onDrop={handleDragDrop}
         onDragOver={handleDragOver}
-        >
-        {
-          tasks.map(task => (
-            task.status === title && <Task task={task} key={task.id} handleDragging={handleDragging}/> 
-          ))
-        }
+      >
+        {tasks.map(
+          (task) =>
+            task.status === title && (
+              <Task task={task} key={task.id} handleDragging={handleDragging} />
+            )
+        )}
       </div>
     </div>
   );
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -3,33 +3,34 @@ import { tasksData } from "../mock/Data";
 import TaskTab from "./TaskTab";
 
 const Tasks = () => {
-
-  const taskStatus = ['todo','in-progress','done'];
-  const[isDraging, setIsDragging] = useState(false);
+  const taskStatus = ["todo", "in-progress", "done"];
+  const [isDraging, setIsDragging] = useState(false);
   const handleDragging = (dragging) => setIsDragging(dragging);
-  const[tasks,setTasks] = useState(tasksData);
+  const [tasks, setTasks] = useState(tasksData);
 
   const handleUpdatedTask = (id, status) => {
+    const task = tasks.find((item) => item.id === id);
 
-    const task = tasks.find(item => item.id === id);
-    
-    if(task && task !== status){
+    if (task && task !== status) {
       task.status = status;
-      setTasks(prevTask => ([
+      setTasks((prevTask) => [
+        ...prevTask.filter((item) => item.id !== id),
         task,
-        ...prevTask.filter(item => item.id !== id)
-      ]))
-  
+      ]);
     }
-    
-  }
+  };
   return (
     <div className="flex flex-grow px-10 mt-4 space-x-6 overflow-auto">
-      {
-          taskStatus.map((status) =>(
-            <TaskTab key={status} tasks={tasks} title={status} handleDragging={handleDragging} isDraging={isDraging}  handleUpdatedTask={handleUpdatedTask} />
-          ))
-      }
+      {taskStatus.map((status) => (
+        <TaskTab
+          key={status}
+          tasks={tasks}
+          title={status}
+          handleDragging={handleDragging}
+          isDraging={isDraging}
+          handleUpdatedTask={handleUpdatedTask}
+        />
+      ))}
       <div className="flex-shrink-0 w-6"></div>
     </div>
   );
