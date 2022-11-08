3dde7483efdd8ca57bb290cc05925a3450007136
safhin <mdjahidhasan920@gmail.com>
2022-11-08 22:48:18 +0600

    API calling issue solved

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -4,15 +4,13 @@ import { useTodo } from "../context/TodoContext";
 import TaskTab from "./TaskTab";
 
 const Tasks = () => {
-  const { todos,loading, error } = useTodo();
+  const { todos, loading, error } = useTodo();
   const taskStatus = ["todo", "in-progress", "done"];
   const [isDraging, setIsDragging] = useState(false);
   const handleDragging = (dragging) => setIsDragging(dragging);
   const [tasks, setTasks] = useState(todos);
 
-
-  const handleUpdatedTask = async(id, status, dragTask, dragOverTask) => {
-
+  const handleUpdatedTask = async (id, status, dragTask, dragOverTask) => {
     const copyTasks = [...tasks];
     const task = tasks.find((item) => item.id === id);
     const index = tasks.indexOf(task);
@@ -22,22 +20,25 @@ const Tasks = () => {
       copyTasks.splice(dragOverTask.current, 0, dragTaskContent);
 
       //api call for update status in backend
-      await fetch(`http://localhost:3366/api/v1/content/task/${id}/${status}`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+      await fetch(`http://localhost:9000/api/v1/content/task/${id}/${status}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           draggedItem: task,
-          draggingPositionId: dragOverTask.current
+          draggingPositionId: dragOverTask.current,
+        }),
+      })
+        .then((response) => {
+          if (response.status === 200) {
+            task.status = status;
+            setTasks(copyTasks);
+            toast.success(`Your task in ${status}`);
+          }
         })
-      })
-      .then(response => {
-        if(response.status === 200){
-          task.status = status;
-          setTasks(copyTasks);
-          toast.success(`Your task in ${status}`);
-        }
-      })
-      .catch(error => console.log(error))
+        .catch((error) => {
+          toast.error("Something wrong happened");
+          console.log(error);
+        });
     } else {
       const [dragTaskContent] = copyTasks.splice(dragTask.current, 1);
       copyTasks.splice(dragOverTask.current, 0, dragTaskContent);
@@ -45,31 +46,24 @@ const Tasks = () => {
     }
   };
 
-  
   return (
     <>
       <ToastContainer className={"justify-end"} />
-
       <div className="flex flex-grow px-10 mt-4 space-x-6 overflow-auto">
-        {
-        !loading && !error && todos && todos.length > 0 && (
+        {!loading && !error && (
           <>
-            {
-              taskStatus.map((status) => (
-                <TaskTab
-                  key={status}
-                  tasks={tasks}
-                  title={status}
-                  handleDragging={handleDragging}
-                  isDraging={isDraging}
-                  handleUpdatedTask={handleUpdatedTask}
-                />
-              ))
-              // <div className="flex-shrink-0 w-6"></div>
-            }
+            {taskStatus.map((status) => (
+              <TaskTab
+                key={status}
+                tasks={tasks}
+                title={status}
+                handleDragging={handleDragging}
+                isDraging={isDraging}
+                handleUpdatedTask={handleUpdatedTask}
+              />
+            ))}
           </>
-        )
-      }
+        )}
       </div>
     </>
   );
