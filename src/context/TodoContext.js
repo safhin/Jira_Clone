9797151e6c5fb073affc801a824b27d13d7495fc
safhin <mdjahidhasan920@gmail.com>
2022-11-11 20:44:03 +0600

    Mock data avaiable for test

--- src/context/TodoContext.js
+++ src/context/TodoContext.js
@@ -1,6 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import api from '../apiUrl.json';
-
+import { tasksData } from "../mock/Data";
 const TodoContext = React.createContext();
 
 export function useTodo() {
@@ -12,28 +11,36 @@ export function TodoProvider({ children }) {
   const [todos, setTodos] = useState([]);
   const [error, setError] = useState(false);
 
+  //if api availabel use this function
+  // useEffect(() => {
+  //   const fetchTodo = async () => {
+  //     await fetch(`${api.baseURL}/content/allTask`)
+  //       .then((response) => {
+  //         if (!response.ok) {
+  //           setError(true);
+  //           setLoading(false);
+  //         }
+  //         return response.json();
+  //       })
+  //       .then((data) => {
+  //         setTodos(data);
+  //         setError(false);
+  //         setLoading(false);
+  //       })
+  //       .catch((error) => {
+  //         console.log(error);
+  //         setError(true);
+  //         setLoading(false);
+  //       });
+  //   };
+  //   fetchTodo();
+  // }, []);
+
+  //this is mock data
   useEffect(() => {
-    const fetchTodo = async () => {
-      await fetch(`${api.baseURL}/content/allTask`)
-        .then((response) => {
-          if(!response.ok){
-            setError(true);
-            setLoading(false);
-          }
-          return response.json();
-        })
-        .then((data) => {
-          setTodos(data);
-          setError(false);
-          setLoading(false);
-        })
-        .catch((error) => {
-          console.log(error);
-          setError(true);
-          setLoading(false);
-        });
-    };
-    fetchTodo();
+    setTodos(tasksData);
+    setLoading(false);
+    setError(false);
   }, []);
 
   const value = {
--- src/mock/Data.js
+++ src/mock/Data.js
@@ -1,50 +1,20 @@
 export const tasksData = [
-    {
-      id:1,
-      title: 'Task 1',
-      description: 'Task 1 description',
-      status: 'todo'
-    },
-    {
-      id:2,
-      title: 'Task 2',
-      description: 'Task 2 description',
-      status: 'todo'
-    },
-    {
-      id:3,
-      title: 'Task 3',
-      description: 'Task 3 description',
-      status: 'todo'
-    },
-    {
-      id:4,
-      title: 'Task 4',
-      description: 'Task 4 description',
-      status: 'todo'
-    },
-    {
-      id:5,
-      title: 'Task 5',
-      description: 'Task 5 description',
-      status: 'in-progress'
-    },
-    {
-      id:6,
-      title: 'Task 6',
-      description: 'Task 6 description',
-      status: 'in-progress'
-    },
-    {
-      id:7,
-      title: 'Task 7',
-      description: 'Task 7 description',
-      status: 'done'
-    },
-    {
-      id:8,
-      title: 'Task 8',
-      description: 'Task 8 description',
-      status: 'done'
-    }
-  ]
+  {
+    id: 1,
+    title: "Task 1",
+    description: "Task 1 description",
+    status: "todo",
+  },
+  {
+    id: 2,
+    title: "Task 2",
+    description: "Task 2 description",
+    status: "in-progress",
+  },
+  {
+    id: 3,
+    title: "Task 3",
+    description: "Task 3 description",
+    status: "done",
+  },
+];
